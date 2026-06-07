341e46f50a59bc2c5f622007548fad35bdee3482
Guilherme-Avellar <guilhermeavellar25@gmail.com>
2026-06-07 20:53:43 -0300

    feat: update sql

--- docs/database-design/DDL.sql
+++ docs/database-design/DDL.sql
@@ -20,6 +20,27 @@ CREATE TABLE Alertas (
     CONSTRAINT pk_alertas PRIMARY KEY (ID)
 );
 
+CREATE TABLE SensorIOT (
+    ID                INT          GENERATED ALWAYS AS IDENTITY,
+    Tipo              VARCHAR(30)  NOT NULL,
+    ERD_PlusCode      VARCHAR(11)  NOT NULL,
+    Status            VARCHAR(15)  NOT NULL,
+    Data_Instalacao   TIMESTAMP    NOT NULL,
+    CONSTRAINT pk_sensor_iot PRIMARY KEY (ID),
+    CONSTRAINT ck_sensor_iot_status CHECK (Status IN ('ATIVO', 'SUSPENSO', 'DESATIVADO'))
+);
+
+CREATE TABLE LeituraSensor (
+    ID               INT           GENERATED ALWAYS AS IDENTITY,
+    Sensor_ID        INT           NOT NULL,
+    Valor            DECIMAL(10,5) NOT NULL,
+    Unidade_Medida   VARCHAR(5)    NOT NULL,
+    Data_Hora        TIMESTAMP     NOT NULL,
+    CONSTRAINT pk_leitura_sensor PRIMARY KEY (ID),
+    CONSTRAINT fk_leitura_sensor_sensor FOREIGN KEY (Sensor_ID)
+        REFERENCES SensorIOT (ID)
+);
+
 CREATE TABLE Enderecos (
     ID            INT         GENERATED ALWAYS AS IDENTITY,
     CNPJ          CHAR(14)    NOT NULL,
@@ -38,12 +59,22 @@ CREATE TABLE RegioesAfetadas (
         REFERENCES Alertas (ID)
 );
 
+CREATE TABLE SensoresAlerta (
+    Alerta_ID   INT NOT NULL,
+    Leitura_ID  INT NOT NULL,
+    CONSTRAINT pk_sensores_alerta PRIMARY KEY (Alerta_ID, Leitura_ID),
+    CONSTRAINT fk_sensores_alerta_alerta FOREIGN KEY (Alerta_ID)
+        REFERENCES Alertas (ID),
+    CONSTRAINT fk_sensores_alerta_leitura FOREIGN KEY (Leitura_ID)
+        REFERENCES LeituraSensor (ID)
+);
+
 CREATE TABLE NotificacoesRecebidas (
     CNPJ       CHAR(14)    NOT NULL,
-    Alertas_ID INT         NOT NULL,
-    CONSTRAINT pk_notificacoes_recebidas PRIMARY KEY (CNPJ, Alertas_ID),
+    Alerta_ID  INT         NOT NULL,
+    CONSTRAINT pk_notificacoes_recebidas PRIMARY KEY (CNPJ, Alerta_ID),
     CONSTRAINT fk_notificacoes_empresa FOREIGN KEY (CNPJ)
         REFERENCES EmpresaAgricola (CNPJ),
-    CONSTRAINT fk_notificacoes_alerta FOREIGN KEY (Alertas_ID)
+    CONSTRAINT fk_notificacoes_alerta FOREIGN KEY (Alerta_ID)
         REFERENCES Alertas (ID)
 );
